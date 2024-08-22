0fe92dcf10f2409561bc0d4d4235df2ecda4959c
can <gurcaglarogulcan@gmail.com>
2024-08-22 17:11:38 +0300

    chore: remove tests

--- tests/test_scan.py
+++
@@ -1,9 +0,0 @@
-from click.testing import CliRunner
-from cli.scan import scan
-
-
-def test_scan():
-    runner = CliRunner()
-    result = runner.invoke(scan, ['--target', '/path/to/code', '--project', 'example_project'])
-    assert result.exit_code == 0
-    assert 'Scanning target: /path/to/code in project: example_project' in result.output
